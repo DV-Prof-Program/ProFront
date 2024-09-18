7c333bd47305acf7db61e8eb547a20ae42b7a8b5
Tesla X <dtvician@icloud.com>
2024-09-18 13:10:24 -0400

    Adding noise-cancelling demo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMuffled = false;
+  audioPlayer: HTMLAudioElement | null = null;
 
+  ngOnInit() {
+    // Get the audio element
+    this.audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
+  }
+
+  toggleMuffle() {
+    if (this.audioPlayer) {
+      if (this.isMuffled) {
+        // Reset to normal sound
+        this.audioPlayer.playbackRate = 1;
+        this.audioPlayer.volume = 1;
+      } else {
+        // Muffle the sound by lowering the volume and playback rate
+        this.audioPlayer.playbackRate = 0.75; // Slow down the audio
+        this.audioPlayer.volume = 0.1; // Lower the volume
+      }
+      this.isMuffled = !this.isMuffled;
+    }
+  }
 }
